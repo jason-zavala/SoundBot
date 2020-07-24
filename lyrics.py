0093e228d531e85bce86f0aa37b62a32059330fa
jason-zavala <jzavala2012@gmail.com>
2020-07-24 17:43:42 -0600

    Update lyrics.py
    comments

--- lyrics.py
+++ lyrics.py
@@ -1,6 +1,6 @@
 import lyricwikia
 
-
+# this is extremely specific, and wont work if there is a typo in the song or author. 
 def get_lyrics(author, song):
     lyrics = lyricwikia.get_lyrics(author, song)
     return lyrics
